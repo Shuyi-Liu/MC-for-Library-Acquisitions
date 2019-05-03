f291bd9e69340cc359fe6a3f5f17ee72f959fd33
JinKyung <jkl4@illinois.edu>
2019-05-02 20:31:59 -0500

    updated

--- preliminary.py
+++ preliminary.py
@@ -96,140 +96,76 @@ def vendor_discount(num_of_titles):
         return 0.02
     elif num_of_titles >= 500:
         return 0.05
-budget = 1000
-space = 500
-plan = get_book_list(10, 1950)
-print('This is plan \n\n', plan, '\n')
-# def select_book(plan, space, budget):
-select_plan = plan.copy(deep=True)
-select_plan['Total_cost_per_book'] = select_plan['Price'] + select_plan['cataloging_cost']
-print('This is select plan\n\n', select_plan, '\n')
 
-select_plan[['cost_accumulate', 'Total_thickness']] = select_plan[['Total_cost_per_book', 'Thickness']].cumsum().where(lambda x: x <= budget).where(lambda x: x <= space)
-acquisition
+def select_book(plan, budget, space):
 
-print('newplan\n\n', select_plan)
-    # print(space_plan)
-    # acquisitions = select_plan.dropna()
-    # print(thick_acquisitions)
-#     return acquisitions
-# a = get_book_list(10, 1950)
-# print(select_book(a, 100, 1000))
-
-# def cost_consider(plan, space, budget):
-#
-#     # plan['Price'] = plan['Price'] * (1 - vendor_discount(num_of_titles))
-#     cost_plan = plan.copy(deep=True)
-#     cost_plan[['Total_price', 'Total_thickness']] = cost_plan[['Price', 'Thickness']].cumsum().where(lambda x: x <= budget).where(lambda x: x <= space)
-#     cost_acquisitions = cost_plan.dropna()
-#     print(cost_acquisitions)
-#     return cost_acquisitions
-# print('This is testing\n\n', cost_consider(a, 100, 100))
-
-#
-# def price_solution(plan):
-#     '''
-#     A function for drop books from a list because of limited budget.
-#     :param plan: Data Frame. A book list with thickness and price
-#     :return: Data Frame excluding the book whose cost is the highest from the plan.
-#     '''
-#     drop = plan['Price'].max()
-#     # print(drop)
-#     new_plan = plan.loc[plan['Price'] != drop]
-#     # print('This is the new book plan:\n', new_plan)
-#     return new_plan
-
-# def MonteCarloSimulation(annual_work_hour, total_volume, budget, space, num_of_titles):
-#     # acquisition_budget = budget - maintenance_cost(annual_work_hour, total_volume) - cataloging_cost(num_of_books, annual_work_hour)
-# num_of_titles = 10
-# budget = 100
-# space = 100
-# annual_work_hour = 1950
-#
-# book_list = get_book_list(num_of_titles)
-# print('booklist\n\n', book_list)
-# plan = book_list.sort_values(by='Demands', ascending=False)
-# print('plansorted\n\n', plan)
-# cost_list = cost_consider(plan, budget)
-# print('costcase\n\n', cost_list)
-# if cost_list['Thickness'].sum() > space:
-#     purchase_list = cost_list
-# else:
-#     purchase_list = space_consider(plan, budget)
-# print('final\n\n', purchase_list)
+    # print('This is plan \n\n', plan, '\n')
+    # def select_book(plan, space, budget):
+    select_plan = plan.copy(deep=True)
+    select_plan['total_cost_per_book'] = select_plan['Price'] + select_plan['cataloging_cost']
+    print('This is select plan\n\n', select_plan, '\n')
+    select_plan['cost_accumulate'] = select_plan['total_cost_per_book'].cumsum().where(lambda x: x <= budget)
+    print('thisi is price\n\n', select_plan)
+    select_plan['Total_thickness'] = select_plan['Thickness'].cumsum().where(lambda x:x <= space)
+    print('This is thickness\n\n', select_plan)
+    acquisitions = select_plan.dropna()
+    return acquisitions
 
 
 
-# space_list = space_consider(plan, budget)
-# print('spacecase\n\n', space_list)
-# cost_list = cost_consider(plan, budget)
-# cost_list = cataloging_cost(plan, budget)
-# num_books = cost_list['Price'].count()
-# cataloging_budget = cataloging_cost(num_books, annual_work_hour)
-# remain = budget - cost_list['Price'].sum()
-    # if cataloging_budget > remain:
-
-    # result_demand = cost_consider(plan, budget)
-    # result_demand = space_consider(plan, space)
-    # num_of_books = result_demand['Price'].count()
+def MonteCarloSimulation(annual_work_hour, total_volume, budget, space, num_of_titles):
+    acquisition_budget = budget - maintenance_cost(annual_work_hour, total_volume)
 
 
+    plan = get_book_list(num_of_titles, annual_work_hour)
 
-    # plan1 = book_list.sort_values(by='Price', ascending=True)
+
+    demand_order = plan.sort_values(by='Demands', ascending=False)
+    price_order = plan.sort_values(by='Price', ascending=True)
+
+    demand_acquisition = select_book(demand_order, acquisition_budget, space)
+    price_acquisition = select_book(price_order, acquisition_budget, space)
+
+    demand_book = demand_acquisition['Price'].count()
+    demand_cost = round(demand_acquisition['total_cost_per_book'].sum() * (1 - vendor_discount(demand_book)), 2)
+    demand_thickness = demand_acquisition['Thickness'].sum()
+
+    price_book = price_acquisition['Price'].count()
+    price_cost = round(price_acquisition['total_cost_per_book'].sum() * 1 - vendor_discount(price_book), 2)
+    price_thickness = price_acquisition['Thickness'].sum()
+
+    print(demand_book, demand_cost, demand_thickness, price_book, price_cost, price_thickness)
+    # print(price_acquisition)
+
+    return demand_book, demand_cost, demand_thickness, price_book, price_cost, price_thickness
+
+print(MonteCarloSimulation(1950, 50000, 1000000000, 10000, 100000))
+
+
+
+if __name__ == '__main__':
+
+
+
+    demand_book = []
+    demand_cost = []
+    demand_thickness = []
+    price_book = []
+    price_cost = []
+    price_thickness = []
+    a = []
+    for i in range(2):
+        b = MonteCarloSimulation(1950, 50000, 1000000000, 10000, 100000)
+        a.append(b)
+    print(a)
+
+    #     num_purchase_book = data['Price'].count()
+    #     total_price = data['Price'].sum()
+    #     all_thickness = data['Thickness'].sum()
+    #     nums.append(num_purchase_book)
+    #     total_costs.append(total_price)
+    #     total_thickness.append(all_thickness)
+    # simulation = pd.DataFrame({'total_purchase_book': nums, 'app_price': total_costs, 'app_space': total_thickness})
     #
-    # resutla = space_consider(plan, num_of_titles, space)
-    # print('a\n\n', resutla)
-    # resutlb = cost_consider(plan, annual_work_hour, total_volume, budget, num_of_titles)
-    # print('b\n\n', resutlb)
-    #
-    # # result1 (price sum howmany bookcount, thickness sum)
-    #
-    # return resutla, resutlb
-
-
-
-
-
-    # acquisition_budget = budget - maintenance_cost(annual_work_hour, total_volume) - cataloging_cost(num_of_titles, annual_work_hour)
-    # print(acquisition_budget)
-    #
-
-    # print(plan)
-    #
-    # if plan['Thickness'].sum() != 0:
-    #     plan['Price'] = plan['Price'] * (1 - vendor_discount(num_of_titles))
-    #     plan['Total_thickness'] = plan['Thickness'].cumsum().where(lambda x: x <= space)
-    #     thick_acquisitions = plan.dropna()
-    #     print('thick case\n', thick_acquisitions)
-    # if plan['Price'].sum() != 0:
-    #     plan['Price'] = plan['Price'] * (1 - vendor_discount(num_of_titles))
-    #     plan['Total_price'] = plan['Price'].cumsum().where(lambda x: x <= acquisition_budget)
-    #     cost_acquisitions = plan.dropna()
-    #     print('price case\n', cost_acquisitions)
-    # # df = pd.DataFrame({'thick': thick_acquisitions, 'cost': cost_acquisitions})
-
-    # return thick_acquisitions, cost_acquisitions
-# annual_work_hour, total_volume, budget, space, num_of_titles
-# a = MonteCarloSimulation(1950, 500000, 3000000, 30000, 100000)
-# print('result\n', a)
-# print(a.count())
-
-#
-# if __name__ == '__main__':
-#
-#     data = MonteCarloSimulation(1950, 500000, 3000000, 300, 1000)
-#     print(data)
-#     nums = []
-#     total_costs = []
-#     total_thickness = []
-#     for i in range(30):
-#         num_purchase_book = data['Price'].count()
-#         total_price = data['Price'].sum()
-#         all_thickness = data['Thickness'].sum()
-#         nums.append(num_purchase_book)
-#         total_costs.append(total_price)
-#         total_thickness.append(all_thickness)
-#     simulation = pd.DataFrame({'total_purchase_book': nums, 'app_price': total_costs, 'app_space': total_thickness})
-#
-#     print(simulation)
+    # print(simulation)
 
